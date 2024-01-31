6fcecf023b1ed22091499a2a7bbcbdfca0c603d6
Christopher Sleeger <csleeger@gmail.com>
2024-01-31 16:52:51 -0400

    Added basic React page for HatList

--- ghi/app/src/App.js
+++ ghi/app/src/App.js
@@ -1,14 +1,20 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainPage from './MainPage';
 import Nav from './Nav';
+import HatList from "./HatList";
 
 function App() {
+  // if (props.hats === undefined) {
+  //   return null;
+  // }
+
   return (
     <BrowserRouter>
       <Nav />
       <div className="container">
         <Routes>
           <Route path="/" element={<MainPage />} />
+          <Route path="/hats" element={<HatList /> } />
         </Routes>
       </div>
     </BrowserRouter>
